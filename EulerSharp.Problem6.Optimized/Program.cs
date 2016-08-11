7f8dbb837dbd4e488011fff0f3e2c652ec2c203f
Stefan Baumann <stefan-baumann@outlook.com>
2016-08-11 22:39:25 +0200

    Problem 7 and optimized version of problem 6

---
+++ EulerSharp.Problem6.Optimized/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EulerSharp.Problem6.Optimized
+{
+    /* Problem Description:
+     * The sum of the squares of the first ten natural numbers is 1^2 + 2^2 + ... + 10^2 = 385
+     * The square of the sum of the first ten natural numbers is (1 + 2 + ... + 10)^2 = 55^2 = 3025
+     * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+     * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+     * 
+     * Solution Notes:
+     * - The sum of the squares of the first x natural numbers is x * (x + 1) * (2 * x + 1) / 6
+     * - The square of the sum of the first x natural numbers (x * (x + 1) / 2)^2 or x^2 * (x + 1)^2 / 4
+     */
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int squareSum = 100 * (100 + 1) * (2 * 100 + 1) / 6; //The sum of the first 100 natural numbers squared individually
+
+            //Calculate the sum of the first 100 natural numbers, squared
+            int sumSquared = 100 * 100 * (100 + 1) * (100 + 1) / 4;
+
+            //Calculate the difference (as the difference is always positive or 0, we use the Math.Abs function to get the absolute value)
+            int difference = Math.Abs(squareSum - sumSquared);
+
+            //Write the result to the console
+            Console.WriteLine("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is " + difference);
+
+            //Wait for a keypress before terminating the program (so that the user can read the output)
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ EulerSharp.Problem7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace EulerSharp.Problem7
+{
+    /* Problem Description:
+     * By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+     * What is the 10 001st prime number?
+     * 
+     * Solution Notes:
+     * - The basic principles on how to find primes is described in the solution to problem #3
+     */
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int primeCount = 1; //The prime number count (1 because we skipped 2
+            int result = -1; //The variable where the 10001st prime will be stored in
+            //Increment the candidate by 2 until the 10001st prime has been found (even numbers are skipped)
+            for (int candidate = 3; primeCount < 10001; candidate += 2)
+            {
+                //Check whether this candidate is prime
+                if (IsPrime(candidate))
+                {
+                    primeCount++; //Increment the prime counter
+                    if (primeCount == 10001)
+                    {
+                        //If this is the 10001st prime, this is the result we were looking for
+                        result = candidate;
+                    }
+                }
+            }
+
+            //Write the result to the console
+            Console.WriteLine("The 10001st prime is " + result);
+
+            //Wait for a keypress before terminating the program (so that the user can read the output)
+            Console.ReadKey(true);
+        }
+
+        public static bool IsPrime(long candidate)
+        {
+            //Loop through every number from 2 to sqrt(candidate) as no number above the square root of the candidate can be a factor of it, if there wasn't one before that
+            for (long test = 2; test * test <= candidate; test++)
+            {
+                if (candidate % test == 0) //Check whether the current number is a factor of the candidate
+                {
+                    return false;
+                }
+            }
+
+            //If it ran through the check successfully it is definitely a prime
+            return true;
+        }
+    }
+}
