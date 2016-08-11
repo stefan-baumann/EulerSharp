946cb3fb383bbd664849c7dc9dbc265122f569aa
Stefan Baumann <stefan-baumann@outlook.com>
2016-08-11 18:49:59 +0200

    Problem 6

---
+++ EulerSharp.Problem6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EulerSharp.Problem6
+{
+    /* Problem Description:
+     * The sum of the squares of the first ten natural numbers is 1^2 + 2^2 + ... + 10^2 = 385
+     * The square of the sum of the first ten natural numbers is (1 + 2 + ... + 10)^2 = 55^2 = 3025
+     * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+     * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+     * 
+     * Solution Notes:
+     * -
+     */
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int squareSum = 0; //The sum of the first 100 natural numbers squared individually
+            int sum = 0; //The sum of the first 100 natural numbers
+
+            //Loop through the first 100 natural numbers
+            for (int n = 1; n <= 100; n++)
+            {
+                squareSum += n * n; //Add the square of the natural number to the squareSum variable
+                sum += n; //Add the natural number itself to the sum variable
+            }
+
+            //Calculate the sum of the first 100 natural numbers, squared
+            int sumSquared = sum * sum;
+
+            //Calculate the difference (as the difference is always positive or 0, we use the Math.Abs function to get the absolute value)
+            int difference = Math.Abs(squareSum - sumSquared);
+
+            //Write the result to the console
+            Console.WriteLine("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is " + difference);
+
+            //Wait for a keypress before terminating the program (so that the user can read the output)
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ EulerSharp.Problem6/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("EulerSharp.Problem6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EulerSharp.Problem6")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("f3284e3a-641d-4338-a97f-7419130a16e6")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion 
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
